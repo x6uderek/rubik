27ce7bcdc22c8117a495cdb7f7864e41dc7d76e6
x6uderek <1056811254@qq.com>
2015-04-20 15:45:47 +0800

    user perspective

---
+++ app/src/main/java/com/derek/android/rubik/bean/UserPerspective.java
@@ -0,0 +1,11 @@
+package com.derek.android.rubik.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by xuran on 2015/4/15.
+ */
+public class UserPerspective implements Serializable {
+    public float xAngle;
+    public float yAngle;
+}
